cfb650bbc9241a90468f5d3da29557f522e296d4
OmarSalhab <omarsalhb24@gmail.com>
2025-04-28 23:54:58 +0300

    feat: update dependencies and configure Tailwind CSS
    - Removed outdated Tailwind CSS Vite plugin and updated Tailwind CSS version.
    - Added react-router-dom for routing capabilities.
    - Introduced autoprefixer and postcss for better CSS handling.
    - Updated App.css to comment out the root styles.
    - Modified App.jsx to include a header and a greeting message.
    - Replaced index.css with Tailwind's @tailwind directives and added custom theme styles.
    - Configured Vite to use Tailwind CSS and autoprefixer in the build process.
    - Created a new postcss.config.js for PostCSS configuration.
    - Added requirements.txt for project setup instructions.
    - Initialized tailwind.config.js for Tailwind CSS configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-
 function App() {
   
 
   return (
     <>
-      
+      <h1>my name is omar</h1>
+      <div>hello world </div>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
   plugins: [react(),tailwindcss()],
   
 })
